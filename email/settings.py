0aac05730a895989e3309ca2e70c4cc1c1be1095
alvin.zing <geniver82@gmail.com>
2013-04-10 23:53:07 +0800

    Initial commit

---
+++ email/settings.py
@@ -0,0 +1,35 @@
+POSITION_APPLIED = ''
+
+RECIPIENT = 'zing.alvin@gmail.com'
+
+# Most likely nothing will change from here onwards.
+
+SENDER = 'zing.alvin@gmail.com'
+
+SUBJECT = 'Job Application for %s' %(POSITION_APPLIED)
+
+PASSWORD = ''
+
+BODY = '''
+<html>
+<head></head>
+<body>
+Dear Sir/Ma'am,
+<br/><br/>
+I am writing to apply for the position of %s. As requested, I have enclosed my resume.
+<br/><br/>
+The opportunity presented in the job description is very interesting and I believe with my strong technical experience, it will make me a very competitive candidate for this position.
+<br/><br/>
+I can be reached anytime via email at zing.alvin@gmail.com or my mobile at 97331846.
+<br/><br/>
+Thank you for your time and consideration. I look forward to speaking with you about this opportunity.
+<br/><br/>
+Yours Sincerely,
+<br/><br/>
+Alvin Zing
+<br/><br/>
+</body>
+</html>
+''' %(POSITION_APPLIED)
+
+CV_LOCATION = '/home/alvin/Downloads/Zing Wai Keong Alvin - Resume (v01).doc'
